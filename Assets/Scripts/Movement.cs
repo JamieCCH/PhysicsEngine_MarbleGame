3d61f53a50230df666eb1ce0a8d43328b460edef
JamieCCH <jamie.cch@gmail.com>
2018-09-10 10:35:31 -0400

    First commit:Preparation for thethe lab class

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] float accel;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.X)) rb.AddForce(new Vector3(accel*Time.deltaTime, 0, 0));
+	}
+}
